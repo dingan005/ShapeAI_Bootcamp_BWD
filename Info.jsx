54dd125c98f3aade7af0ba1248ecf11ace60a7bc
DIMAL THOMAS <85438037+dingan005@users.noreply.github.com>
2021-06-28 14:57:57 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+  return(
+    <div class="note">
+    Javascript and React.js   
+      This was an amazing bootcamp taken up by Shaurya Sinha
+      We covered everything from Scratch including Javascript React.js,HTML.
+    </div>
+  );
+}
+
+export default Info;
